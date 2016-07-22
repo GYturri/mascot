21df43b11f64c143c873c386ab117e25e52a8e31
Sebastian <rvstiven@gmail.com>
2016-07-22 13:53:35 +0000

    avance de sebastian en el diseño de la pagina de inicio

--- pets/forms.py
+++ pets/forms.py
@@ -4,6 +4,30 @@ from .models import Pet
 from usuarios.models import Usuario
 
 class NPetForm(forms.ModelForm):
+	name = forms.CharField(widget=forms.TextInput(attrs={
+			'class': 'form-control',
+			'placeholder': 'Nombres',
+			'required': '',
+		}))
+		
+	birth = forms.CharField(widget=forms.TextInput(attrs={
+			'class': 'form-control',
+			'placeholder': 'Nombres',
+			'required': '',
+		}))	
+		
+	photo = forms.ImageField(widget=forms.TextInput(attrs={
+			'type': 'file',
+			'placeholder': 'Nombres',
+			'required': '',
+		}))	
+		
+	raza = forms.CharField(widget=forms.TextInput(attrs={
+			'class': 'form-control',
+			'placeholder': 'Nombres',
+			'required': '',
+		}))	
+		
 	class Meta:
 		model = Pet
 		exclude = ("id","usuario","estatus","estate")
